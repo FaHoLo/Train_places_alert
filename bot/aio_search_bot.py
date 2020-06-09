f92003b5a280c8ad133cf9c87a5b72d042958c2f
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-06-09 23:20:30 +0300

    Move no-sandbox upper

--- bot/aio_search_bot.py
+++ bot/aio_search_bot.py
@@ -136,10 +136,10 @@ async def check_search(search):
 async def make_rzd_request(url):
     # ChromeBrowser (heroku offical supports it), easy guide: https://youtu.be/Ven-pqwk3ec?t=184)
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), options=chrome_options)
     # driver = webdriver.Firefox(executable_path='C:\Program Files\Mozilla Firefox\geckodriver')
     try:
