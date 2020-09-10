1c9ffbe46d4cdc3a509a5da090097fdd65ed05fd
FaHoLo <47195343+FaHoLo@users.noreply.github.com>
2020-09-10 21:42:14 +0300

    Add docstrings and typing

--- bot/hunter.py
+++ bot/hunter.py
@@ -1,10 +1,26 @@
+"""Place hunter bot module.
+
+Bot needs environment varibles:
+    GOOGLE_CHROME_BIN: Chrome browser path.
+    CHROMEDRIVER_PATH: Chrome driver path (easy guide: https://youtu.be/Ven-pqwk3ec?t=184).
+    TG_PROXY: Bot proxy (default = None).
+    TG_BOT_TOKEN: Bot token.
+    TG_LOG_BOT_TOKEN Telegram log bot token.
+    TG_LOG_CHAT_ID: Telegram log chat id.
+    DB_HOST: Redis database host.
+    DB_PORT: Redis database port.
+    DB_PASS: Redis database password.
+"""
+
 import asyncio
 import datetime
 from itertools import product
 import os
+from textwrap import dedent
+from typing import Optional, Tuple, Union, List
 # import re
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from dotenv import load_dotenv
 from redis.exceptions import TimeoutError
 from selenium import webdriver
@@ -27,12 +43,14 @@ separator = DIGIT_GROUPING_SEPARATORS[0]
 
 
 def main():
+    """Run place hunter."""
     place_hunt = asyncio.get_event_loop()
     place_hunt.create_task(start_searching())
     place_hunt.close()
 
 
 async def start_searching():
+    """Run place hunter main task."""
     while True:
         try:
             searches = await collect_searches()
@@ -45,7 +63,12 @@ async def start_searching():
         await asyncio.sleep(5)
 
 
-async def collect_searches():
+async def collect_searches() -> dict:
+    """Collect searches from db.
+
+    Returns:
+        searches: Collected searches.
+    """
     search_keys = await collect_search_keys()
     searches = {}
     for search_key in search_keys:
@@ -57,6 +80,11 @@ async def collect_searches():
 
 
 async def collect_search_keys():
+    """Collect search keys by search patterns from db.
+
+    Returns:
+        keys: Search keys.
+    """
     keys = []
     search_patterns = ['tg-*', 'vk-*']
     for search_pattern in search_patterns:
@@ -68,11 +96,16 @@ async def collect_search_keys():
             return keys
         if search_keys:
             keys.extend(search_keys)
-        asyncio.sleep(2)
+        await asyncio.sleep(2)
     return keys
 
 
-async def search_places(searches):
+async def search_places(searches: dict) -> None:
+    """Search places in searches and notify user about its appearance.
+
+    Args:
+        searches: Active searches of all users.
+    """
     for search_id, search_info in searches.items():
         if search_info.get('price_limit') is None:
             continue
@@ -83,32 +116,51 @@ async def search_places(searches):
         await asyncio.sleep(5)
 
 
-async def check_search(search):
+async def check_search(search: dict) -> Optional[str]:
+    """Check single user search for places and return answer.
+
+    Args:
+        search: User search info.
+
+    Returns:
+        answer: Answer to send to user.
+    """
     train_numbers = [
         train_number.strip()
         for train_number in search['train_numbers'].strip().split(', ')
     ]
     response = await make_rzd_request(search['url'])
     if not response:
-        return
+        return None
     await asyncio.sleep(2)
     trains_with_places, trains_that_gone, trains_without_places = await collect_trains(response)
     if trains_with_places == 'Bad url':
         return 'Битая ссылка. Скорее всего, неверная дата. Прочитай /help и начни новый поиск'
     if not trains_with_places and not trains_that_gone and not trains_with_places:
-        return
-    answer = await check_for_wrong_train_numbers(train_numbers, trains_with_places,
-                                                 trains_that_gone, trains_without_places)
+        return None
+
+    # TODO Its anti-pattern here, change it
+    answer = await check_for_wrong_train_numbers(
+        train_numbers, trains_with_places, trains_that_gone, trains_without_places)  # type: ignore
     if answer:
         return answer
-    answer = await check_for_places(train_numbers, trains_with_places, int(search['price_limit']))
+    answer = await check_for_places(
+        train_numbers, trains_with_places, int(search['price_limit']))  # type: ignore
     if answer:
         return answer
-    answer = await check_for_all_gone(train_numbers, trains_that_gone)
+    answer = await check_for_all_gone(train_numbers, trains_that_gone)  # type: ignore
     return answer
 
 
-async def make_rzd_request(url):
+async def make_rzd_request(url) -> Optional[str]:
+    """Get response from rzd with Selenium.
+
+    Args:
+        url: Search url.
+
+    Returns:
+        response: Page data.
+    """
     # ChromeBrowser (heroku offical supports it), easy guide: https://youtu.be/Ven-pqwk3ec?t=184)
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
@@ -138,26 +190,33 @@ async def make_rzd_request(url):
             # no longer running, so ChromeDriver is assuming that Chrome has crashed.)
             # You don't need it in logs so just print to know, it happend, but you can try
             # to solve it, if they are too often there
-            # It takes about 1-2 secs from starting of webdriver to its error autodetection
+            # It takes about 1-2 secs from starting of webdriver to this error autodetection
             # and handling
             print(text)
         else:
             await utils.handle_exception(LOG_BOT, LOGGER_NAME, text=delta_msg)
-        return
-    except Exception as ex:
-        await utils.handle_exception(LOG_BOT, LOGGER_NAME, text=ex.msg)
-        return
+        return None
+    except Exception:
+        await utils.handle_exception(LOG_BOT, LOGGER_NAME)
+        return None
 
     try:
         driver.get(url)
+        # We must close driver if any exception raised, to save RAM
     except TimeoutException:
         driver.close()
-        return
+        return None
     except Exception as ex:
-        await LOG_BOT.send_message(os.environ.get('TG_LOG_CHAT_ID'), ex.msg)
         await utils.handle_exception(LOG_BOT, LOGGER_NAME)
         driver.close()
-        return
+
+        try:
+            # ex.msg check is here coz sometimes driver dont write it in traceback
+            await LOG_BOT.send_message(os.environ.get('TG_LOG_CHAT_ID'), ex.msg)  # type: ignore
+        except Exception:
+            pass
+        return None
+
     await asyncio.sleep(2)
     while True:
         data = driver.page_source
@@ -168,8 +227,24 @@ async def make_rzd_request(url):
     return data
 
 
-async def collect_trains(data):
+async def collect_trains(data: str) -> Union[
+        Tuple[List[Tag], List[str], List[str]],
+        Tuple[str, None, None]]:
+    """Collect all trains type from search page data.
+
+    Train types: train with places, train that gone, train without places
+
+    Args:
+        data: Search page data.
+
+    Returns:
+        trains_with_places: Trains that have vacant places.
+                            Tag type of trains for subsequent selections.
+        trains_that_gone: Trains that gone.
+        trains_without_places: Trains without vacant places.
+    """
     if data.find('за пределами периода') != -1:
+        # TODO raise error except that weird return
         return 'Bad url', None, None
 
     soup = BeautifulSoup(data, 'html.parser')
@@ -196,8 +271,22 @@ async def collect_trains(data):
     return trains_with_places, trains_that_gone, trains_without_places
 
 
-async def check_for_wrong_train_numbers(train_numbers, trains_with_places,
-                                        trains_that_gone, trains_without_places):
+async def check_for_wrong_train_numbers(
+            train_numbers: List[str], trains_with_places: List[Tag],
+            trains_that_gone: List[str], trains_without_places: List[str]
+        ) -> Optional[str]:
+    """Check train numbers for collected trains entry.
+
+    Args:
+        train_numbers: Train numbers from user search.
+        trains_with_places: Trains that have vacant places.
+                            Tag type of trains for subsequent selections.
+        trains_that_gone: Trains that gone.
+        trains_without_places: Trains without vacant places.
+
+    Returns:
+        answer: Answer about bad train numbers.
+    """
     status = 'Not found'
     for train_number in train_numbers:
         for train in trains_with_places:
@@ -220,24 +309,55 @@ async def check_for_wrong_train_numbers(train_numbers, trains_with_places,
         if len(train_numbers) == 1:
             return 'Неверный номер поезда, не нашел его в списках на эту дату. Прочитай /help и начни новый поиск'
         return 'Неверные номера поездов, не нашел ни одного в списках на эту дату. Прочитай /help и начни новый поиск'
+    return None
 
 
-async def check_for_places(train_numbers, trains_with_places, price_limit):
-    # time_pattern = r'route_time\">\d{1,2}:\d{2}'
+async def check_for_places(train_numbers: List[str], trains_with_places: List[Tag],
+                           price_limit: int) -> Optional[str]:
+    r"""Check trains for vacant places with price limit.
+
+    If price limit == 1 check is not performed.
+
+    For development use in case of broked time selector:
+    time_pattern = r'route_time\">\d{1,2}:\d{2}'
+    time = re.search(time_pattern, train_data)[0][-5:]
+
+    Args:
+        train_numbers: Train numbers from user search.
+        trains_with_places: Trains that have vacant places.
+                            Tag type of trains for selections.
+        price_limit: Price limit for place price.
+
+    Returns:
+        Answer: Answer about finding suitable places.
+    """
     for train_data, train_number in product(trains_with_places, train_numbers):
         if train_number not in str(train_data):
             continue
         time = train_data.select_one('span.train-info__route_time').text.strip()
-        # re.search(time_pattern, train_data)[0][-5:]
         if price_limit == 1:
             return f'Нашлись места в поезде {train_number}\nОтправление в {time}'
         price = await check_for_satisfying_price(train_data, price_limit)
         if price:
-            price = await put_spaces_into_price(price)
-            return f'Нашлись места в поезде {train_number}\nЦена билета: {price} ₽\nОтправление в {time}'
+            spaced_price = await put_spaces_into_price(price)
+            return dedent(f'''\
+            Нашлись места в поезде {train_number}
+            Цена билета: {spaced_price} ₽
+            Отправление в {time}
+            ''')
+    return None
 
 
-async def check_for_satisfying_price(train_data, price_limit):
+async def check_for_satisfying_price(train_data: Tag, price_limit: int) -> Optional[int]:
+    """Check train place for satisfying prices.
+
+    Args:
+        train_data: Tag of train data.
+        price_limit: Price limit of search.
+
+    Returns:
+        price: Satisfying place price.
+    """
     global separator
     for span_price in train_data.select('span.route-cartype-price-rub'):
         try:
@@ -247,20 +367,40 @@ async def check_for_satisfying_price(train_data, price_limit):
             price = int(span_price.text.strip().encode('UTF-8').replace(separator, b''))
         if price <= price_limit:
             return price
+    return None
 
 
-async def put_spaces_into_price(price):
-    price = str(price)
+async def put_spaces_into_price(price: int) -> str:
+    """Put spaces into price.
+
+    Args:
+        price: Place price.
+
+    Returns:
+        new_price: Price with spaces.
+    """
+    new_price = str(price)
     price_parts = []
-    while len(price) > 3:
-        price_parts.append(price[-3:])
-        price = price[:-3]
-    price_parts.append(price)
+    while len(new_price) > 3:
+        price_parts.append(new_price[-3:])
+        new_price = new_price[:-3]
+    price_parts.append(new_price)
     price_parts.reverse()
-    return ' '.join(price_parts)
+    new_price = ' '.join(price_parts)
+    return new_price
 
 
-async def check_for_all_gone(train_numbers, trains_that_gone):
+async def check_for_all_gone(train_numbers: List[str],
+                             trains_that_gone: List[str]) -> Optional[str]:
+    """Check train numbers for entry in gone trains.
+
+    Args:
+        train_numbers: Train numbers from user search.
+        trains_that_gone: Trains that gone.
+
+    Returns:
+        answer: Answer about the departure of all searched trains.
+    """
     gone_trains = []
     for train, train_number in product(trains_that_gone, train_numbers):
         if train_number not in train:
@@ -268,6 +408,7 @@ async def check_for_all_gone(train_numbers, trains_that_gone):
         gone_trains.append(train_number)
     if len(gone_trains) == len(train_numbers):
         return 'Места не появились, все поезда ушли 😔'
+    return None
 
 
 if __name__ == "__main__":
